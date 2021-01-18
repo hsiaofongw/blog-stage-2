83370ebf928a27a9693b3dbd3e08b9affb5afae2
hsiaofongw <hsiaofong.w@gmail.com>
2021-01-18 17:22:07 +0800

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#073642`,
         theme_color: `#073642`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.jpg`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
