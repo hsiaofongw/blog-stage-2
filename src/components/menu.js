d481c860e53b75dd6712ca86e680368313095c49
hsiaofongw <hsiaofong.w@gmail.com>
2021-03-05 05:42:05 +0800

    update

--- src/components/menu.js
+++ src/components/menu.js
@@ -8,6 +8,7 @@ export default function Menu() {
         <Link to="/">文章</Link> 
         <Link to="/search/">检索</Link> 
         <Link to="/friends/">友链</Link> 
+        <Link target="_blank" to="https://travellings.now.sh">开往</Link> 
         <Link to="/ca/">自动机</Link> 
         <Link to="/about/">关于</Link>
         </div>
