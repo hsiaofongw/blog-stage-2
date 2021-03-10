4b6045ab62d8affb857ba1cc8311e9945d62bc9f
hsiaofongw <hsiaofong.w@gmail.com>
2021-03-10 10:12:09 +0800

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,31 +4,5 @@ module.exports = {
         description: `在探索中前进……`,
         author: `@hsiaofongw`,
     },
-    plugins: [
-        `gatsby-plugin-react-helmet`,
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `images`,
-                path: `${__dirname}/src/images`,
-            },
-        },
-        `gatsby-transformer-sharp`,
-        `gatsby-plugin-sharp`,
-        {
-            resolve: `gatsby-plugin-manifest`,
-            options: {
-                name: `gatsby-starter-default`,
-                short_name: `starter`,
-                start_url: `/`,
-                background_color: `#073642`,
-                theme_color: `#073642`,
-                display: `minimal-ui`,
-                icon: `src/images/favicon.jpg`, // This path is relative to the root of the site.
-            },
-        },
-        // this (optional) plugin enables Progressive Web App + Offline functionality
-        // To learn more, visit: https://gatsby.dev/offline
-        // `gatsby-plugin-offline`,
-    ],
+    plugins: []
 }
